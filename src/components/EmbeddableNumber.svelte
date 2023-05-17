3fce5d55310965824ed3e7002cb8779eb57584d5
Heechul Ryu <ryuhcii@gmail.com>
2023-05-16 21:25:38 -0700

    Dry verbose Number embedding

---
+++ src/components/EmbeddableNumber.svelte
@@ -0,0 +1,14 @@
+<!-- @component
+Wrap Number to work better with astro mdx files
+-->
+
+<script lang="ts">
+  import Number from './Number.svelte';
+  import OpacitySkeleton from './parts/OpacitySkeleton.svelte';
+</script>
+
+<div class="not-prose">
+  <OpacitySkeleton>
+    <Number />
+  </OpacitySkeleton>
+</div>
