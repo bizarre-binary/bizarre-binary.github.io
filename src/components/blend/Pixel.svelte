b7258dc1793a50091c4db91eae0addd683e8c4f9
Heechul Ryu <ryuhcii@gmail.com>
2023-05-26 00:10:51 -0700

    Add copy to clipboard feature on Pixel

--- src/components/blend/Pixel.svelte
+++ src/components/blend/Pixel.svelte
@@ -4,6 +4,7 @@ Pixel that contains sub pixels
 <script context="module" lang="ts">
   import { createEventDispatcher } from 'svelte';
   import SubPixel from './SubPixel.svelte';
+  import CopyToClipboard from '@components/parts/CopyToClipboard.svelte';
   import { toHex } from '@lib/convert';
   import type { RGBA } from '@lib/convert';
 
@@ -61,20 +62,21 @@ Pixel that contains sub pixels
   <div class="grow" />
 </div>
 <div class="my-5">
-  <label
-    for={undefined}
-    class="block focus-within:outline outline-none rounded flex items-center p-0.5 mx-0.1"
-  >
+  <div class="focus-within:outline outline-none rounded flex items-center p-0.5 mx-0.1">
     <div class="grow" />
-    <!-- an "hack" for square - https://stackoverflow.com/a/19068538/1570165 -->
-    <div
-      class="rounded outline outline-gray-200 block w-0 h-0 pr-[3.5rem] pb-[3.5rem] mx-0.3"
-      style={`background-color: ${colorHex};`}
-    />
-    <div class="mx-2">
-      <pre class="text-xs text-left">{colorNumbers}</pre>
-      <pre class="text-3xl text-center">{colorHex}</pre>
-    </div>
+    <CopyToClipboard textToCopy={colorHex}>
+      <div class="flex">
+        <!-- an "hack" for square - https://stackoverflow.com/a/19068538/1570165 -->
+        <div
+          class="rounded outline outline-gray-200 block w-0 h-0 pr-[3.5rem] pb-[3.5rem] mx-0.3"
+          style={`background-color: ${colorHex};`}
+        />
+        <div class="mx-2">
+          <pre class="text-xs text-left">{colorNumbers}</pre>
+          <pre class="text-3xl text-center">{colorHex}</pre>
+        </div>
+      </div>
+    </CopyToClipboard>
     <div class="grow" />
-  </label>
+  </div>
 </div>
---
+++ src/components/parts/CopyToClipboard.svelte
@@ -0,0 +1,88 @@
+<!-- @component
+A wrapper for copy to clipboard function
+-->
+<script context="module" lang="ts">
+  import { onMount } from 'svelte';
+</script>
+
+<script lang="ts">
+  export let textToCopy = '';
+  let showFeedback = false;
+  let copyStatus = true;
+
+  let actualCopiedText = '';
+
+  const success = 'success';
+  const error = 'error';
+
+  $: feedback = copyStatus
+    ? { type: success, message: `\`${actualCopiedText}\` copied!`, duration: 2000 }
+    : {
+        type: error,
+        message: 'failed to copy to clipboard',
+        duration: 3000,
+      };
+
+  let ccElement: HTMLElement;
+  let timeoutId: ReturnType<typeof setTimeout> | undefined;
+
+  const giveFeedback = (status: boolean) => {
+    copyStatus = status;
+    showFeedback = true;
+
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      showFeedback = false;
+    }, feedback.duration);
+  };
+
+  const validateCopying = (event: Event) => {
+    if (event.target !== ccElement) {
+      giveFeedback(false);
+    } else {
+      const valueAttr = ccElement.attributes.getNamedItem('value');
+      if (valueAttr && valueAttr.value === textToCopy) {
+        actualCopiedText = valueAttr.value;
+        giveFeedback(true);
+      } else {
+        giveFeedback(false);
+      }
+    }
+  };
+
+  const clickEvt = new Event('click');
+  const copyText = async () => {
+    if (ccElement) {
+      ccElement.dispatchEvent(clickEvt);
+    } else {
+      giveFeedback(false);
+    }
+  };
+
+  $: tooltipText = showFeedback ? feedback.message : 'click to copy';
+
+  onMount(() => {
+    //TODO maybe there is a way to prevent importing again for no good reason
+    import('@github/clipboard-copy-element');
+  });
+</script>
+
+<div
+  class="mb-2 tooltip tooltip-bottom tooltip-success tooltip-error before:mt-0.8 after:mt-1 hover:before:opacity-80 hover:after:opacity-80"
+  class:tooltip-success={showFeedback && feedback.type === success}
+  class:tooltip-error={showFeedback && feedback.type === error}
+  data-tip={tooltipText}
+>
+  <clipboard-copy
+    class="hidden"
+    bind:this={ccElement}
+    value={textToCopy}
+    on:clipboard-copy={validateCopying}>Copy</clipboard-copy
+  >
+  <button class="hover:outline outline-gray rounded p-1" on:click={copyText}>
+    <slot />
+  </button>
+</div>
