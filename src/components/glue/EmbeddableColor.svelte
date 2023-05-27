a9856fad9bb1ce6029721fcee9e84c9794f909d0
Heechul Ryu <ryuhcii@gmail.com>
2023-05-26 17:52:22 -0700

    Show Color Picker on the index page

---
+++ src/components/glue/EmbeddableColor.svelte
@@ -0,0 +1,14 @@
+<!-- @component
+Wrap Color to work better with astro mdx files
+-->
+
+<script lang="ts">
+  import Color from '../Color.svelte';
+  import OpacitySkeleton from '../parts/OpacitySkeleton.svelte';
+</script>
+
+<div class="not-prose">
+  <OpacitySkeleton>
+    <Color />
+  </OpacitySkeleton>
+</div>
