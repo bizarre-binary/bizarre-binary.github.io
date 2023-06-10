30a74088c7b457d2ccfa699cc4c0e012f9c82969
Heechul Ryu <ryuhcii@gmail.com>
2023-06-10 14:39:29 -0700

    Add tooltip on IPv4Input to mitigate weak feedback
    such as:
    - no keyboard showing up on mobile devices like Safari

--- src/components/parts/IPv4Input.svelte
+++ src/components/parts/IPv4Input.svelte
@@ -18,6 +18,7 @@ A convenient addition to allow pasting/editing as whole
   let button: HTMLButtonElement;
 
   const selectText = (e: Event) => {
+    // on mobile Safari (Webkit), keyboard will not show up unlike other "normal" browsers - https://github.com/jquery-archive/jquery-mobile/issues/3016
     const elm = e.target as HTMLInputElement;
     elm.select();
   };
@@ -68,16 +69,21 @@ A convenient addition to allow pasting/editing as whole
     class="absolute font-mono text-xl lt-sm:text-2xl lt-xs:text-lg text-gray-600 left-11 top-1 lt-sm:top-3 lt-xs:top-4 lt-sm:left-9 lt-xs:left-6 z-30 w-[88%]"
     class:hidden={!show}
   >
-    <input
-      class="invalid:bg-yellow-100 w-full px-1 text-center rounded border border-gray sm:tracking-widest"
-      type="text"
-      bind:this={input}
-      bind:value={inputAsWhole}
-      {placeholder}
-      {pattern}
-      on:focus={selectText}
-      on:change={evaluate}
-      on:blur={onInputBlur}
-    />
+    <div
+      class="tooltip before:text-xs before:mb-1 after:mb-1 tooltip-open mb-20 tooltip-accent w-full"
+      data-tip={'select/copy/paste/edit'}
+    >
+      <input
+        class="invalid:bg-yellow-100 w-full px-1 text-center rounded border border-gray sm:tracking-widest"
+        type="text"
+        bind:this={input}
+        bind:value={inputAsWhole}
+        {placeholder}
+        {pattern}
+        on:focus={selectText}
+        on:change={evaluate}
+        on:blur={onInputBlur}
+      />
+    </div>
   </div>
 </div>
