654bec6d16c35379d877834edd040dfc994a8ca0
Heechul Ryu <ryuhcii@gmail.com>
2023-06-10 17:39:37 -0700

    Revert floating descriptions on IPv4Network

--- src/components/blend/IPv4Network.svelte
+++ src/components/blend/IPv4Network.svelte
@@ -59,18 +59,19 @@ to augment IPv4 with Network and Netmask visualization
 <div class="relative">
   <small class="absolute text-gray-400 w-full">
     <div class="relative">
-      <div class="absolute w-full h-full overflow-clip z--10">
+      <div class="absolute w-full h-full">
         <div class="h-full bg-gray-200" style:width={`${(prefix / 32) * 100}%`} />
       </div>
-      <div class="mix-blend-colo/r">
-        <pre class="inline-block ml-2">Netmask:</pre>
-        <div class="inline-block">
-          <PartialTransition notation={renderedMask} fstDelimiter={' = '} />
-        </div>
-      </div>
+      <pre class="opacity-0">placeholder</pre>
     </div>
   </small>
 </div>
+<small class="absolute text-gray-400 w-full">
+  <pre class="inline-block ml-2">Netmask:</pre>
+  <div class="inline-block">
+    <PartialTransition notation={renderedMask} fstDelimiter={' = '} />
+  </div>
+</small>
 <small><pre class="opacity-0">placeholder</pre></small>
 <div class="relative">
   <div class="absolute w-full h-full rounded-b overflow-clip z--10">
@@ -92,16 +93,14 @@ to augment IPv4 with Network and Netmask visualization
     />
   </div>
 </div>
-<div class="relative mb-1">
-  <small class="absolute mx-2 text-gray-600 mt-0.6">
-    <pre class="inline-block text-cyan">Network:</pre>
-    <div class="inline-block text-cyan">
-      <PartialTransition notation={renderedNetwork} />
-    </div>
-    <pre class="text-gray-400 inline-block lt-xs:hidden">= Address & Netmask</pre>
-  </small>
-  <small><pre class="opacity-0">placeholder</pre></small>
-</div>
+<small class="absolute mx-2 text-gray-600 mt-0.6">
+  <pre class="inline-block text-cyan">Network:</pre>
+  <div class="inline-block text-cyan">
+    <PartialTransition notation={renderedNetwork} />
+  </div>
+  <pre class="text-gray-400 inline-block lt-xs:hidden">= Address & Netmask</pre>
+</small>
+<small><pre class="opacity-0 mb-1">placeholder</pre></small>
 <Bits
   hideHeader={true}
   integer={network}
